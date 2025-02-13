6c7d6c7337bf6380f6a341d52d9212685c6952b2
Val Cubeto <valcubetaa@gmail.com>
2025-02-13 17:09:57 -0300

    init

---
+++ src/frontend/index.js
@@ -0,0 +1,8 @@
+let burger = document.getElementById("burger");
+let profile = document.getElementById("profile");
+let view = document.getElementById("view");
+let options = document.getElementById("options");
+
+burger.addEventListener("click", function () {
+  burger.classList.toggle("open");
+});
