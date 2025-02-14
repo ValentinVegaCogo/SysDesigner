47e27c94fe7e7397157bbd9b244d1da99555d118
Val Cubeto <valcubetaa@gmail.com>
2025-02-14 03:36:23 -0300

    test

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,1 +1,22 @@
 /// @ts-nocheck
+
+const headers_html = { headers: { "Content-Type": "text/html" } }
+const headers_js = { headers: { "Content-Type": "application/javascript" } }
+const headers_css = { headers: { "Content-Type": "text/css" } }
+
+Bun.serve({
+  hostname: "localhost",
+  port: 3000,
+  static: {
+    "/index.html": Response.redirect("/"),
+    "/": new Response(await Bun.file("../frontend/index.html").bytes(), headers_html),
+    "/res/index.js": new Response(await Bun.file("../frontend/index.js").bytes(), headers_js),
+    "/res/styles.css": new Response(await Bun.file("../frontend/styles.css").bytes(), headers_css),
+    "/res/prism.css": new Response(await Bun.file("../frontend/prism/prism.css").bytes(), headers_css),
+    "/res/prism.js": new Response(await Bun.file("../frontend/prism/prism.js").bytes(), headers_js),
+  },
+  fetch(request: Request) {
+    console.info(request.method, request.url, "not handled")
+    return new Response("Not found", { status: 404 })
+  }
+})
--- src/frontend/index.js
+++ src/frontend/index.js
@@ -6,3 +6,43 @@ let options = document.getElementById("options");
 burger.addEventListener("click", function () {
   burger.classList.toggle("open");
 });
+
+window.escapeHtml = function(code) {
+  return code
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;");
+}
+
+window.getOutputElem = function (elem) {
+  return elem.parentNode.getElementsByTagName("code")[0];
+}
+
+window.updateCodeOutput = function (elem, code) {
+  let out = window.getOutputElem(elem);
+  if (code[code.length - 1] === "\n") {
+    code += " ";
+  }
+  out.innerHTML = window.escapeHtml(code);
+  window.Prism.highlightElement(out);
+}
+
+window.onCodeInput = function (elem, event) {
+  if (event.key === "Tab") {
+    event.preventDefault();
+    let code = elem.value;
+    let bef = code.slice(0, elem.selectionStart);
+    let aft = code.slice(elem.selectionEnd, code.length);
+    let cursorPos = elem.selectionEnd + 4;
+    elem.value = bef + "    " + aft;
+    elem.selectionStart = cursorPos;
+    elem.selectionEnd = cursorPos;
+    updateCodeOutput(elem, elem.value);
+  }
+}
+
+window.syncCodeScroll = function (elem) {
+  let out = window.getOutputElem(elem);
+  out.scrollTop = elem.scrollTop;
+  out.scrollLeft = elem.scrollLeft;
+}
